0b74e431b7b7530c37c885bc9eff4ea8a116d025
Jama Mohamed <jama3137@gmail.com>
2020-01-01 11:36:07 +0300

    Finished with 404

--- src/pages/UnknownRoute.jsx
+++ src/pages/UnknownRoute.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import UnknownRouteLogo from "../assets/UnknownRoute.svg"
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 export const UnknownRoute = () => {
   return (
-    <div>
-      this is a 404
+    <div className="container h-100 d-flex justify-content-center">
+      <div className="my-auto">
+        <Container>
+          <Row>
+            <Col className="align-self-center"><h1>I think you're lost</h1></Col>
+            <Col className="align-self-center" xs="auto"><Image style={{ width: "400px", height: "400px" }} src={UnknownRouteLogo}></Image></Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   )
 }
